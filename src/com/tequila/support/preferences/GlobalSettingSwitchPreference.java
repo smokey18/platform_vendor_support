cd06300defd2a66b55c83b56e01e8641af9719c0
Anushek Prasal <anushekprasal@gmail.com>
2020-03-20 13:04:56 +0530

    support: Add and use SwitchPreference

--- src/com/tequila/support/preferences/GlobalSettingSwitchPreference.java
+++ src/com/tequila/support/preferences/GlobalSettingSwitchPreference.java
@@ -18,7 +18,6 @@ package com.tequila.support.preferences;
 
 import android.content.Context;
 import android.provider.Settings;
-import androidx.preference.SwitchPreference;
 import android.util.AttributeSet;
 
 public class GlobalSettingSwitchPreference extends SwitchPreference {
