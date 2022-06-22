Havoc-OS/android_vendor_support

support: Import Custom switch and category
Layouts taken from our OnePlus extras repo
https://github.com/DirtyUnicorns/android_device_oneplus_extras/commit/8dea4ae33f201947b5f59440aa21c3a57dadd3f0

Change-Id: I95368d5b9ac0c1764dc977b9c049c6bfd4b2c24c
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

src/com/havoc/support/preferences/PreferenceCategory.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.havoc.support.R;
+
+public class PreferenceCategory extends androidx.preference.PreferenceCategory {
+
+    public PreferenceCategory(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+
+    public PreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+}

smokey18/platform_vendor_support

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

src/com/tequila/support/preferences/PreferenceCategory.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tequila.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.tequila.support.R;
+
+public class PreferenceCategory extends androidx.preference.PreferenceCategory {
+
+    public PreferenceCategory(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+
+    public PreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_category_material_settings);
+    }
+}